679959ebc11fbf4ad9d357a4325ab00499fb003a
Tyrone <tyrone.cloma0219@gmail.com>
2022-07-21 16:32:52 -0400

    added styling on focus screen

--- src/Components/FighterSceen.js
+++ src/Components/FighterSceen.js
@@ -1,8 +1,10 @@
-const FighterScreen = ({ selectedFighter }) => {
+const FighterScreen = ({ selectedFighter, fighters }) => {
   return (
-    <div id='fighter-screen'>
-      <img src={''} alt='fullscreen fighter' className="char-model"/>
+    <div id='fighter-screen' style={{ backgroundImage: `url(https://www.smashbros.com/assets_v2/img/fighter/${selectedFighter.toLowerCase()}/bg.jpg)`, backgroundSize: 'cover'}}>
+      {/* <button className="close-focus"> X </button> */}
+      <img src={`https://www.smashbros.com/assets_v2/img/fighter/${selectedFighter.toLowerCase()}/main.png`} alt='fullscreen fighter' className="char-model"/>
       <h3 className="char-name"> {selectedFighter} </h3>
+      <h3 className="char-name-bg"> {selectedFighter} </h3>
     </div>
   )
 }
